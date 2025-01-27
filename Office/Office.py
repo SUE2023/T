6f75303efbfaa49d1544829cec17046bf019c454
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-01-27 19:50:41 +0000

    Applicaton

---
+++ Office/Office.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Application Module"""
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import create_app, db
+from app.models import User, CalendarEvent
+
+app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "sa": sa,
+        "so": so,
+        "db": db,
+        "User": User,
+        "CalendarEvent": CalendarEvent
+    }
